49429cb55d7f1fbc9e16841d914f3630f5ac4ee7
torone89 <torone89>
2022-09-19 16:43:23 +0200

    finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,2 @@
 @extends('layouts.main')
 @section('title', 'home')
-
-
-@section('main-content')
-
-    {{-- <ul>
-        @foreach ($movies as $movie)
-            <li> <img src="{{ $movie->cover }}" alt="{{ $movie->title }}">
-            <li> {{ $movie->original_title }}</li>
-            <li> {{ $movie->date }}</li>
-        @endforeach
-    </ul> --}}
-
-@endsection
